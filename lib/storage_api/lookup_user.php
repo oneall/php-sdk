57d6611d88829515453b7db4b3b58047517de721
cschlesser <cschlesser@oneall.com>
2015-11-19 16:50:23 +0100

    User Lookup

--- lib/storage_api/lookup_user.php
+++ lib/storage_api/lookup_user.php
@@ -19,27 +19,19 @@
 // HTTP Handler and Configuration
 include '../assets/config.php';
 
-// Users \ Create a new user in your cloud storage database
-// https://docs.oneall.com/api/resources/storage/users/create-user/
-
-// Identity Structure
-// https://docs.oneall.com/api/basic/identity-structure/
+// User Cloud Storage \ User Lookup
+// https://docs.oneall.com/api/resources/storage/users/lookup-user/
 
 // Message Structure
 $structure = array(
 	'request' => array(
 		'user' => array(
-			'login' => 'johnny',
-			'password' => null,
-			'externalid' => null,
-			'identity' => array(
-				'name' => array(
-					'givenName' => 'John',
-					'familyName' => 'Doe' 
-				) 
-			) 
+			'externalid' => 100000002,
+			'login' => 'john',
+			'password' => 'iaaU3WA6kFlR' 
 		) 
-	) 
+	)
+	 
 );
 
 // Encode structure
@@ -49,20 +41,28 @@ echo "<h1>Data</h1>";
 echo "<pre>" . stripslashes (oneall_pretty_json::format_string ($structure_json)) . "</pre>";
 
 // Make Request
-$oneall_curly->post (SITE_DOMAIN . "/storage/users.json", $structure_json);
+$oneall_curly->post (SITE_DOMAIN . "/storage/users/user/lookup.json", $structure_json);
 $result = $oneall_curly->get_result ();
 
 // Success
-if ($result->http_code == 201)
+if ($result->http_code == 200)
 {
-	echo "<h1>Success " . $result->http_code . " (New user created)</h1>";
-	echo "<pre>" . oneall_pretty_json::format_string ($result->body) . "</pre>";
+	echo "<h1>Success " . $result->http_code . " (User found)</h1>";
+}
+// Auth Error
+elseif ($result->http_code == 401)
+{
+	echo "<h1>Error " . $result->http_code . " (Invalid Credentials)</h1>";
+}
+// Not found
+elseif ($result->http_code == 404)
+{
+	echo "<h1>Error " . $result->http_code . " (No User found)</h1>";
 }
 // Error
 else
 {
 	echo "<h1>Error " . $result->http_code . "</h1>";
-	echo "<pre>" . oneall_pretty_json::format_string ($result->body) . "</pre>";
 }
-
+echo "<pre>" . oneall_pretty_json::format_string ($result->body) . "</pre>";
 ?>
